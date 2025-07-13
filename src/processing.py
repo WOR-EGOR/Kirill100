b3dcbf9906d5c4467708ae089307660fcb8ba05f
Kirill <akatev278aye@mail.com>
2025-07-13 20:05:26 +0300

    Home work done

---
+++ src/processing.py
@@ -0,0 +1,10 @@
+def filter_by_state(dicta, state="EXECUTED"):
+    new_dict = []
+    for value in dicta:
+        if value.get("state") == state:
+            new_dict.append(value)
+    return new_dict
+
+
+def sort_by_date(dicta, sta=True):
+    return sorted(dicta, key=lambda item: item["date"], reverse=True)
