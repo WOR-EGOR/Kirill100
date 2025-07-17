52741a25d85f37410c879cf082d394e981eb2141
Kirill <akatev278aye@mail.com>
2025-07-17 20:42:39 +0300

    done 2

--- src/processing.py
+++ src/processing.py
@@ -1,12 +1,12 @@
-def filter_by_state(dicta, state="EXECUTED"):
+def filter_by_state(privet, state="EXECUTED"):
     """Функция фильтрует слово по умолчанию, и формирует список"""
     new_dict = []
-    for value in dicta:
+    for value in privet:
         if value.get("state") == state:
             new_dict.append(value)
     return new_dict
 
 
-def sort_by_date(dicta, sta=True):
+def sort_by_date(privet, sta=True):
     """сортирует словарь по убыванию"""
-    return sorted(dicta, key=lambda item: item["date"], reverse=True)
+    return sorted(privet, key=lambda item: item["date"], reverse=True)
