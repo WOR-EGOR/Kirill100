8dfdbd02e3e4124b603359b188d3125b42f199d5
Kirill <akatev278aye@mail.com>
2025-07-17 20:27:35 +0300

    done 1

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,5 @@
 def filter_by_state(dicta, state="EXECUTED"):
+    """Функция фильтрует слово по умолчанию, и формирует список"""
     new_dict = []
     for value in dicta:
         if value.get("state") == state:
@@ -7,4 +8,5 @@ def filter_by_state(dicta, state="EXECUTED"):
 
 
 def sort_by_date(dicta, sta=True):
+    """сортирует словарь по убыванию"""
     return sorted(dicta, key=lambda item: item["date"], reverse=True)
